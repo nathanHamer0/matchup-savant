82b97d9c94050e116c655e1442b53c6190909346
Nathan Hamer <nathanhamer396@gmail.com>
2026-01-11 16:03:49 -0500

    Rough commit 5

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from . import matchup, data_loader, name_utils
+import matchup
+import data_loader
+import name_utils
 
 app = FastAPI(title="Matchup Savant")
 
